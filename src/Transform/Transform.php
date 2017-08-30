4b0e06b01034f5b72600e52b69fdbd071d11a11e
alice.tong <961799948@qq.com>
2017-08-31 00:21:42 +0800

    init

---
+++ src/Transform/Transform.php
@@ -0,0 +1,17 @@
+<?php
+namespace Transform;
+
+include_once __DIR__.'/../../vendor/autoload.php';
+
+use houdunwang\str\Str;
+
+class MyTransform
+{
+    //转换中文数组到拼音数组
+    public function ChineseArrToPinyinArr($arr = array()){
+        foreach($arr as $key => $item){
+            $arr[$key] = Str::pinyin($item);
+        }
+        return $arr;
+    }
+}
